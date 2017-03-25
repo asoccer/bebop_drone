50b15a6c4729e4998d4ea1d09db1b7f658b0d87d
Adam D <reainasky@gmail.com>
2017-03-25 04:40:13 -0400

    script turns off when "stop" is sent from client

--- server.py
+++ server.py
@@ -2,9 +2,12 @@
 
 import socket,sys
 
+
+
 s = socket.socket()
-host = socket.gethostname() #This assigns the 
+host = socket.gethostname()
 port = 12397
+
 s.bind(('',port))
 
 s.listen(5)
@@ -12,9 +15,15 @@ s.listen(5)
 while True:
 	try:
 		c,addr = s.accept()
+		data = c.recv(1024)
 		print "Got connection from", addr
+		print "data received ->", data
+
 		c.send("received thank you")
 		c.close()
+		if(data == "stop"):
+			sys.exit()	
+		
 	except:
 		print "thank you closing"
 		sys.exit()
