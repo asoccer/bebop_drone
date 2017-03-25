8c68f7b04be12e1b3159ebac7a0368a687c2fdfe
Adam D <reainasky@gmail.com>
2017-03-25 03:37:39 -0400

    method created to just "send" a message.

--- client.py
+++ client.py
@@ -1,13 +1,16 @@
 import socket
-TCP_IP = '192.168.50.133'
-PORT = 5005
-BUFFER= 1024
-MESSAGE = "hello world"
 
-s = socket.socket()
-host = TCP_IP
-port = 12397
+def send_message(MESSAGE):
 
-s.connect((host,port))
-print s.recv(1024)
-s.close()
+	PORT = 5005
+	BUFFER= 1024 #Lower for faster response
+	s = socket.socket()
+	host = '192.168.50.133'
+	port = 12397 
+	s.connect((host,port))
+	s.send(MESSAGE)
+	data = s.recv(1024)
+	print data
+	s.close()
+
+send_message("ayylmao")
